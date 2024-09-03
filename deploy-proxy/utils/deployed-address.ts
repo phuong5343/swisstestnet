da0b5ab7f872fa2c2e55809816391bc7b35de333
phuong5343 <158416558+phuong5343@users.noreply.github.com>
2024-09-03 02:01:37 +0000

    task6

---
+++ deploy-proxy/utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x20E77b74f9C218214EE31d3E1201Df0120aB2D3e'
+export const ProxyAdmin = '0xDc5db9c883f6ab9d76ABA3D3E684CF1Ad3Bba00B'
+export const SWTRImplementation = '0xCd5f6bC50861715D9dbE8523f4d7E4844572996A'
+    
